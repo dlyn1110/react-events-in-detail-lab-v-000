c392880aa9f340e30384541f7dbf3e763b1d3f0f
Dani Lyn <dlyn1110@gmail.com>
2019-04-23 23:27:31 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,14 +1,14 @@
 // Code DelayedButton Component Here
 import React, { Component } from 'react'
 
-export default class CoordinatesButton extends Component {
-
+export default class DelayedButton extends Component {
   handleClick = (event) => {
-    let x = event.clientX
-    let y = event.clientY
-    let coordinates = [x, y]
-    this.props.onReceiveCoordinates(coordinates)
+    event.persist();
+    window.setTimeout(() => {
+      this.props.onDelayedClick(event)
+    }, this.props.delay)
   }
+
   render() {
     return (<button onClick={this.handleClick} />)
   }
